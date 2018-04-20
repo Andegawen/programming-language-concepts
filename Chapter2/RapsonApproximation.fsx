5cc8d6122a5448f51def69865b798deb806def5a
krzysztof.kroczak <krzysztof.kroczak@red-gate.com>
2018-04-20 16:46:45 +0100

    clean Rapson

--- Chapter2/RapsonApproximation.fsx
+++ Chapter2/RapsonApproximation.fsx
@@ -1,24 +1,31 @@
 //a(next)= (a_i + n/a_i)/2
 
-let next n x: float = (x+n/x)/2.0
+//get approximation
+let next n a_i: float = (a_i+n/a_i)/2.0
 
+//compute approximations indifinietly
 let rec repeat (func:float->float) (a0:float) : seq<float>   = 
     let a1 = func a0;
     seq {
         yield a1
         yield! repeat func a1 
      }
-     
+
+//limit sequence to find the result
 let rec within (eps:float) (sequence:seq<float>) :float =
     let first = Seq.head sequence;
     let rest = Seq.skip 1 sequence
     let second = Seq.head rest;
     if abs(first-second) <= eps then second else within eps rest
 
+//square root
 let magicSqrt (n:float) a0 eps = within eps (repeat (next n) a0)
 let Number = 9.0;
-magicSqrt Number (Number/2.0) 0.01 |> printfn "%A"    
+magicSqrt Number (Number/2.0) 0.01 |> printfn "%A"
 
+
+
+//conventional approach of square root
 let rec sqrtStep (x : float) (current : float) (eps: float) : float = 
     let nxt = (next x current)
     if abs(nxt - current) < eps then nxt else (sqrtStep x nxt eps)
